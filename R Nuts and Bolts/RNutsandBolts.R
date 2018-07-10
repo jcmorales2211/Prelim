2cbb5d12177163ff93e8c7fa79c8626a8c271e89
jcmorales2211 <jcmorales2211@gmail.com>
2018-07-10 15:43:09 +0800

    RNutsandBolts

---
+++ R Nuts and Bolts/RNutsandBolts.R
@@ -0,0 +1,58 @@
+x<- 1:20
+x
+x <- 1.3L
+x<- 1.34
+x
+x<- c(0.5, 0.6)
+x
+x<- c(TRUE, FALSE)
+x
+x<- c(T, F)
+x
+x<- c("a", "b","c")
+x <- vector("numeric", length = 10)
+x
+y<-c(1.7,"a")
+y
+y<-c(TRUE,2)
+y
+y<- c("a", TRUE)
+y
+y
+x<- c("a", "b", "c")
+as.numeric(x)
+as.logical(x)
+as.complex(x)
+x<-0.6
+x<-0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+x<- list(1,"a", TRUE, 1+ 4i)
+x
+m<- matrix(nrow =2, ncol =3)
+m
+dim(m)
+attributes(M)
+attributes(m)
+m<- matrix(1:6, norw =2, ncol =3)
+m<- matrix(1:6, nrow =2, ncol =3)
+m
+m<- 1:10
+m
+dim(m)
+dim(m)<- c(2,5)
+m
+x<- 1:3
+y<- 10:12
+cbind <-10;12
+cbind (x,y)
+rbind(x,y)
+x<- factor(c("yes", "yes", "no", "yes", "no"))
+x
+table(X)
+table(x)
+unclass(x)
+x<-factor(c("yes", "yes", "no", "yes", "no"),levels = c("yes","no"))
+                     
